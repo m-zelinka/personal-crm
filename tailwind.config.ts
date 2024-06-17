eb9a899df75e156ce3e9dc9943de7f3cbd902a9c
Marek Zelinka <mzelinka17@gmail.com>
2024-06-17 16:17:20 +0200

    setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,19 @@
+import formsPlugin from "@tailwindcss/forms";
 import type { Config } from "tailwindcss";
+import colors from "tailwindcss/colors";
+import defaultTheme from "tailwindcss/defaultTheme";
 
 export default {
   content: ["./app/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["InterVariable", ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        gray: colors.neutral,
+      },
+    },
   },
-  plugins: [],
+  plugins: [formsPlugin],
 } satisfies Config;
